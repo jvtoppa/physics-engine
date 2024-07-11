b4d4d0f7d5f2117aa22b4e3aaa8865666d3a129b
João Vitor Toppa <jvtoppa@gmail.com>
2024-07-11 18:50:27 -0300

    Update ballistic.py
    now working properly

--- ballistic.py
+++ ballistic.py
@@ -1,41 +1,37 @@
 from OpenGL.GLUT import *
 from OpenGL.GL import *
+from OpenGL.GLU import *
 import sys
 from veclib import vec3
 from particle import particle
-import datetime
 
 class Bullet:
     def __init__(self, part):
         self.particle = part
 
+
     def render(self):
+
+        self.simUpdate()
+        glClear(GL_COLOR_BUFFER_BIT)
         glColor3f(2,2,2)
         glPushMatrix()
         glTranslatef(self.particle.pos.a, self.particle.pos.b, self.particle.pos.c)
-        glutSolidSphere(0.01, 20, 20)
-        glPopMatrix()
-
-        glColor3f(0.75, 0.75, 0.75)
-        glPushMatrix()
-        glTranslatef(self.particle.pos.a, 0, self.particle.pos.c)
-        glScalef(1.0, 0.1, 1.0)
-        glutSolidSphere(0.01, 20, 20)
+        glutSolidSphere(0.05, 40, 40)
         glPopMatrix()
         glFlush()
+        glutPostRedisplay()
+    def simUpdate(self):
+        duration = 0.01
+        self.particle.integrate(duration)
+        duration+= 1
 
-def fire(bullet):
-    bullet.particle.setPos(vec3(-2,0.2,0))
-    bullet.particle.setVel(vec3(2,0,0))
-    bullet.particle.setAcc(vec3(0,-0.05,0))
-    bullet.particle.setDamping(0.99)
-    bullet.particle.setinvMass(1/2)
-
-def simUpdate(bullet):
-    duration = 0.01
-    bullet.particle.integrate(duration)
-    #duration += 1/1000
-    #print(duration)
+    def fire(self, x, y):
+        self.particle.setPos(vec3(x,y,0))
+        self.particle.setVel(vec3(0.5,0.5,0))
+        self.particle.setAcc(vec3(0,-0.3,0))
+        self.particle.setDamping(0.99)
+        self.particle.setinvMass(1/2)
 
 glutInit(sys.argv)
 
@@ -44,11 +40,16 @@ glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
 
 glutCreateWindow(b"Ballistic Demo")
 glViewport(1,1,1000,1000)
-shot = Bullet(particle())
+glMatrixMode( GL_PROJECTION )
+glLoadIdentity()
+w = glutGet( GLUT_WINDOW_WIDTH ) / 300.0
+h = glutGet( GLUT_WINDOW_HEIGHT ) / 300.0
+glOrtho( -1 * w, 1 * w, -1 * h, 1 * h, 10, -10)
 
-fire(shot)
-while True:
-    
-    simUpdate(shot)
-    print(shot.particle)
-    shot.render()
+glMatrixMode( GL_MODELVIEW )
+glLoadIdentity(); 
+shot = Bullet(particle())
+shot.fire(-2,1)
+
+glutDisplayFunc(shot.render)
+glutMainLoop()
